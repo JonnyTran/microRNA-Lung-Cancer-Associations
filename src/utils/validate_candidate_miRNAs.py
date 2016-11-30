64a4aa0c7b6421a6554df448073a84d8a7f557f4
JonnyTran <Dumamay69>
2016-11-30 03:03:26 -0600

    added src/utils/validate_candidate_miRNAs by percentage of known NSCLC miRNAs

---
+++ src/utils/validate_candidate_miRNAs.py
@@ -0,0 +1,14 @@
+
+def percent_candidate_in_validated(candidate_miRNAs, validated_miRNAs):
+    """
+    Verify a model by percentages of known miRNAs found in experimentally
+     validated databases (e.g. miRTarBase)
+
+    :param candidate_miRNAs: a list of selected miRNAs code names
+    :param validated_miRNAs: a list of validated miRNAs code names
+    :return: a percentage
+    """
+    return float(len(set(candidate_miRNAs) & set(validated_miRNAs))) / len(validated_miRNAs)
+
+def intersection_miRNA(candidate_miRNAs, validated_miRNAs):
+    return set(candidate_miRNAs) & set(validated_miRNAs)
